2c12c1bb7d48fb240f04884f330882239cb3334a
Sammy Valencia <Sammyvalencia1116231@correo.itm.edu.co>
2026-04-08 22:10:57 -0500

    Ultimo Avance

--- src/main/java/com/teniscol/shoestore/configuration/DocumentationConfig.java
+++ src/main/java/com/teniscol/shoestore/configuration/DocumentationConfig.java
@@ -1,10 +1,15 @@
-package com.teniscol.shoestore.configuration;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class DocumentationConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new io.swagger.v3.oas.models.info.Info()
+                                .title("API para el sistema de una tienda de tenis")
+                                .version("1.0")
+                                .description("Esta es una API para el desarrollo de nuestro sistema.")
+                                .contact(new Contact().name("Grupo 6am").email("Grupo-6am@programacionSoftware.com"))
+                );
+    }
 }
