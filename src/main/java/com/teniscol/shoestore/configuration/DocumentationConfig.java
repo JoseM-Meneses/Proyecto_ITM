9e6a0ac1314299a87514372c8403d0ea56ed8c06
Sammy Valencia <Sammyvalencia1116231@correo.itm.edu.co>
2026-04-08 22:26:16 -0500

    Actualizar DocumentationConfig y borrar init.sql

--- src/main/java/com/teniscol/shoestore/configuration/DocumentationConfig.java
+++ src/main/java/com/teniscol/shoestore/configuration/DocumentationConfig.java
@@ -1,3 +1,10 @@
+package com.teniscol.shoestore.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class DocumentationConfig {
 
@@ -8,8 +15,8 @@ public class DocumentationConfig {
                         new io.swagger.v3.oas.models.info.Info()
                                 .title("API para el sistema de una tienda de tenis")
                                 .version("1.0")
-                                .description("Esta es una API para el desarrollo de nuestro sistema.")
-                                .contact(new Contact().name("Grupo 6am").email("Grupo-6am@programacionSoftware.com"))
-                );
+                                .description("Esta es una API para el desarollo de nuestro sistema.")
+                                .contact(new Contact().name("Grupo 6am").email("Grupo-6am@programacionSoftware.com")));
     }
+
 }
